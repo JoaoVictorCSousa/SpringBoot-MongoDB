4350c46f427535b0e931266f17a23e3dc9be4f5e
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2023-09-20 15:16:42 -0300

    feat: add exception class and getting user by id

---
+++ src/main/java/com/joao/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.joao.workshopmongo.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.joao.workshopmongo.services.exception.ObjectNotFoundException;
+
+//Classe para tratar possiveis erros nas requisições
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+
+}
