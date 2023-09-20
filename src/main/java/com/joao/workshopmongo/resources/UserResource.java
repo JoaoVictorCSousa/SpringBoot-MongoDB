efa7b80e916b8bbc37c8ba352a9cd27b9131aa14
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2023-09-20 12:03:29 -0300

    feat: add userResource userService and userRepository classes and interface and connecting mongodb to repository and service

--- src/main/java/com/joao/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joao/workshopmongo/resources/UserResource.java
@@ -1,15 +1,15 @@
 package com.joao.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joao.workshopmongo.domain.User;
+import com.joao.workshopmongo.services.UserService;
 
 //Recurso Rest
 @RestController
@@ -17,12 +17,12 @@ import com.joao.workshopmongo.domain.User;
 @RequestMapping(value="/users")
 public class UserResource {
 	
-	@RequestMapping(method=RequestMethod.GET)  //Ou pode fazer @GetMapping
+	@Autowired
+	private UserService service;
+	
+	@GetMapping 
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Silva", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");	
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
